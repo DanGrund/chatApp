3b3f38c945b920f6bfbdfad438e36b4d8eb9f245
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-12-20 14:53:40 -0700

    begin tests

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -4,6 +4,7 @@ import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
 
 import Application from '../lib/components/Application';
+import ChatWindow from '../lib/components/ChatWindow';
 
 describe('Application', () => {
 
@@ -12,8 +13,20 @@ describe('Application', () => {
     assert.equal(wrapper.type(), 'div');
   });
 
-  it.skip('contains a chat window', ()=> {
+  it.skip('contains a chat window', () => {
     const wrapper = shallow(<Application />)
-    assert.equal(wrapper.contains(), '')
-  })
+    assert.equal(wrapper.contains(<ChatWindow />), true);
+  });
+
+  it('is able to append a new message to the screen', () => {
+    var spy = sinon.spy(addNewMessage)
+    assert.isFunction(wrapper.contains(addNewMessage),
+
+    // const wrapper = shallow(<Application />);
+  });
+
+  it.skip('can get a user array', () => {
+    const wrapper = shallow(<Application />);
+    assert.isFunction(wrapper.contains(getUserArray));
+  });
 });
